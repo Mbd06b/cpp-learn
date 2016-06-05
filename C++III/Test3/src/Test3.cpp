af1ab0178dee9a7a74a7c8414e815048e0769aa7
Matthew Dowell <mbd06b@gmail.com>
2016-06-05 16:25:29 -0500

    correcting workspace settings

--- C++III/Test3/src/Test3.cpp
+++ C++III/Test3/src/Test3.cpp
@@ -1,6 +1,6 @@
 //============================================================================
-// Name        : TEST2.cpp
-// Author      : 
+// Name        : Test3.cpp
+// Author      : mbd06b
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
